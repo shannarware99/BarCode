8667b08043826929646c3d8591e32d90f673ce12
Shantanu <shanror98@gmail.com>
2022-09-06 20:33:34 +0530

    forgot and reset password

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,20 @@ class User < ApplicationRecord
   def generate_jwt
     JWT.encode({ id: id, exp: 360.minute.from_now.to_i }, ENV['DEVISE_JWT_SECRET_KEY'])
   end
+
+  def generate_password_token!
+    self.reset_password_token = generate_jwt
+    self.reset_password_sent_at = Time.now.utc
+    save!
+  end
+
+  def password_token_valid?
+    (self.reset_password_sent_at + 4.hours) > Time.now.utc
+  end
+
+  def reset_password!(password)
+    self.reset_password_token = nil
+    self.password = password
+    save!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
     post   'users/sign_up'  => '/api/users#sign_up'
     post   'users/sign_in'  => '/api/users#sign_in'
     delete 'users/sign_out' => '/api/users#sign_out'
-
+    
+    post 'users/forgot' => '/api/users#forgot'
+    post 'users/reset' => '/api/users#reset'
     resources :business
   end
 
